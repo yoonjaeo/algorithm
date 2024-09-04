04b42b8f22f68f417a83f84a9b4d6a36d623670a
yoonjaeo <140726268+yoonjaeo@users.noreply.github.com>
2024-09-04 19:34:07 +0900

    [level 2] Title: 주식가격, Time: 34.95 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 17.4 MB, 시간: 31.87 ms
+메모리: 19.4 MB, 시간: 34.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 09일 20:08:26
+2024년 09월 04일 19:34:04
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,38 +1,19 @@
+from collections import deque
 def solution(prices):
-    answer = []
-    prices_delta = []
+    answer = [0] * len(prices)
+    price_q = deque()
     
-    answer = [0]*len(prices)
-    
-    for idx, price in enumerate(prices):
-        # print(prices_delta)
+    for e, price in enumerate(prices):
+        if len(price_q) == 0 :
+            price_q.append((e, price))
+        elif price_q[-1][1] > price:
+            while price_q and price_q[-1][1] > price :
+                q_e, q_price = price_q.pop()
+                answer[q_e] = e - q_e
+        price_q.append((e, price))
 
-        if len(prices_delta) != 0 and prices_delta[-1][1] > price:
-            for price_d in prices_delta[::-1]:
-                last_idx, last_price = price_d
-                if last_price > price :
-                    answer[last_idx] = idx - last_idx
-                    prices_delta.pop()
-        prices_delta.append((idx,price))
-                    
-    
-    for idx in range(len(prices[:-1])):
-        if answer[idx] == 0:
-            answer[idx] = len(prices) - (idx + 1)
-            
-            
-    
-    
-#     for i, price in enumerate(prices[:-1]):
-#         j = i + 1
-#         for next_p in prices[i+1:]:
-#             if price > next_p:
-#                 answer.append(j-i)
-#                 break
-#             if j == len(prices)-1:
-#                 answer.append(j-i)
-#             j += 1
-#     answer.append(0)
-            
         
+    for e, price in price_q:
+        answer[e] = len(prices) - e -1
+                
     return answer
